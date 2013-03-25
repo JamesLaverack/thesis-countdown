ae665f71359ff58add11f8b3d5dff8e13c4a75b6
James Laverack <neoncode@gmail.com>
2013-03-25 16:56:12 +0000

    Add some JS to do custom extentions

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,33 @@
+var true_deadline = new Date("May 17, 2013 23:59:00");
+var deadline = true_deadline;
+$('#no-ext-button').hide();
+$('#custom-ext-controls input').keyup(function() {
+    
+    var txt = $('#custom-ext-controls input').val();
+    var num = parseInt(txt);
+    // Reset
+    $('#custom-ext-group').removeClass("error warning success");
+    $('#custom-ext-controls span').text("");
+    $('custom-ext-submit').addClass("disabled");
+    if (txt=="") {
+        // Do nothing
+    } else if (isNaN(num)) {
+        $('#custom-ext-group').addClass("error");
+        $('#custom-ext-controls span').text("That's not a number.");
+    } else if (num<0) {
+        $('#custom-ext-group').addClass("warning");
+        $('#custom-ext-controls span').text("Very funny, positive numbers only.");
+    } else {
+        $('#custom-ext-group').addClass("success");
+        $('#custom-ext-submit').removeClass("disabled");
+    }
+});
+
 function cdtd() {
-    var deadline = new Date("May 17, 2013 23:59:00");
+    update_countdown()
+    var timer = setTimeout('cdtd()',1000);
+}
+function update_countdown(){
     var now = new Date();
     var timeDiff = deadline.getTime() - now.getTime();
     if (timeDiff <= 0) {
@@ -19,13 +47,27 @@ function cdtd() {
         minutes %= 60;
         seconds %= 60;
         
-    //     document.getElementById("daysBox").innerHTML = days;
-    //     document.getElementById("hoursBox").innerHTML = hours;
-    //     document.getElementById("minsBox").innerHTML = minutes;
-    //     document.getElementById("secsBox").innerHTML = seconds;
         $('#countdown h1').text(weeks + ' Weeks, ' + days + ' Days, ' + hours + ' Hours.');
         $('#countdown h2').text('and ' + minutes + ' Minutes, ' + seconds + ' Seconds.');
-        $('#waking-hours').text("That's only about " + waking_hours + " waking hours left. You could slep less, but try not to burn out.");
-        var timer = setTimeout('cdtd()',1000);
+        $('#waking-hours').text("That's only about " + waking_hours + " waking hours left. You could slep less, but try not to burn out."); 
     }
+}
+function extend_deadline(extention_in_hours) {
+    deadline = new Date(true_deadline.getTime() + 1000*60*60*extention_in_hours);
+    if(extention_in_hours==0) {
+        $('#no-ext-button').fadeOut();
+    } else {
+        $('#no-ext-button').fadeIn();
+    }
+    update_countdown();
+}
+
+function validate_custom() {
+    alert("herp");
+}
+
+function custom_extend_deadline() {
+    var txt = $('#custom-ext-controls input').val();
+    extention_in_hours = parseInt(txt);
+    extend_deadline(extention_in_hours);
 }
